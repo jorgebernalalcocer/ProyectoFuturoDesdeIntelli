ddd5e8ba25625840fe6d45c3368e636b1db5e36a
jorgebernalalcocer <j.90.bernal@gmail.com>
2021-11-12 18:00:07 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int value1= 7;
+        int value2= 60;
+
+        System.out.println("Estudiante "+ args[0]);
+
+        public static String veredicto(String valor1, String valor2) {
+            if(valor1.equals(valor2)){
+                return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+            }
+            if ( interger.valueOf(valor1)<Interger.valueOf(valor2)){
+                return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+
+            }else{
+                return "Ideal. Trabajas los contenidos en casa.";
+            }
+        }
+
+
+    }
+}
